592669672ac5874ee745253b5ff1882192cbcbdd
Krisna Okky Widayat <117734262+CarlitosXe@users.noreply.github.com>
2024-03-11 08:24:32 +0700

    Add files via upload

---
+++ Praktikum 3/Tugas Tambahan/MenuTidakTersediaException.java
@@ -0,0 +1,9 @@
+//Nama  : Krisna Okky Widayat
+//NIM   : 24060122140105
+
+public class MenuTidakTersediaException extends Exception {
+    public MenuTidakTersediaException(String message) {
+        super(message);
+    }
+}
+
